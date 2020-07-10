17703fe88a42c42376ae9258d9f26a71a86003c6
Justin Merz <jrmerz@gmail.com>
2020-07-10 09:29:09 -0700

    first pass at vessel deployment

--- cmds/pull.sh
+++ cmds/pull.sh
@@ -11,8 +11,8 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $ROOT_DIR/..
 source config.sh
 
-for image in "${ALL_DOCKER_IMAGES[@]}"; do
-  docker pull $UCD_LIB_DOCKER_ORG/$image:$DOCKER_CACHE_TAG || true
+for image in "${ALL_DOCKER_BUILD_IMAGES[@]}"; do
+  docker pull $image:$DOCKER_CACHE_TAG || true
 done
 
 docker pull docker.io/library/maven:3-jdk-8-slim
