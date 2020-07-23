42f33f9ad94b4c26518f076522cd567d0c63ef9c
Justin Merz <jrmerz@gmail.com>
2020-07-23 16:51:14 -0700

    fix for docker build cache, adding check in deployment

--- gcloud/dc-deploy.sh
+++ gcloud/dc-deploy.sh
@@ -8,6 +8,11 @@ echo "USER:$USER";
 echo "BRANCH_NAME:${BRANCH_NAME}";
 echo "CLUSTER_NAME:${CLUSTER_NAME}";
 
+if [[ -d "/opt/${CLUSTER_NAME}" ]]; then
+  echo "Docker compose cluster /opt/${CLUSTER_NAME} is does not exist, skipping deployment"
+  exit 0
+fi
+
 cd /opt/${CLUSTER_NAME}
 git reset HEAD --hard
 git checkout ${BRANCH_NAME}
