cbeeffd384cbedcd6dd708eeba3266f2128ebdb9
Justin Merz <jrmerz@gmail.com>
2020-07-24 10:27:35 -0700

    quick fix

--- gcloud/dc-deploy.sh
+++ gcloud/dc-deploy.sh
@@ -8,7 +8,7 @@ echo "USER:$USER";
 echo "BRANCH_NAME:${BRANCH_NAME}";
 echo "CLUSTER_NAME:${CLUSTER_NAME}";
 
-if [[ -d "/opt/${CLUSTER_NAME}" ]]; then
+if [[ ! -d "/opt/${CLUSTER_NAME}" ]]; then
   echo "Docker compose cluster /opt/${CLUSTER_NAME} is does not exist, skipping deployment"
   exit 0
 fi
