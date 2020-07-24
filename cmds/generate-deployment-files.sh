6b50ed7fc16b3b088b2eafeecb1a3a14da428697
Justin Merz <jrmerz@gmail.com>
2020-07-24 11:32:51 -0700

    removing k8s files, exposing fuseki port for main deployment

--- cmds/generate-deployment-files.sh
+++ cmds/generate-deployment-files.sh
@@ -25,11 +25,6 @@ echo "$content" > ../docker-compose.yaml
 # generate k8s files
 mkdir -p ../$GKE_CONFIG_DIR
 
-if [[ -z $INTERNAL_IP ]]; then
-  echo "IP address ($IP_NAME) not set for branch $BRANCH_NAME in config.sh"
-  exit -1;
-fi
-
 INDEXER_IMAGE_NAME_ESCAPED=$(echo $INDEXER_IMAGE_NAME | sed 's/\//\\\//g')
 API_IMAGE_NAME_ESCAPED=$(echo $API_IMAGE_NAME | sed 's/\//\\\//g')
 GATEWAY_IMAGE_NAME_ESCAPED=$(echo $GATEWAY_IMAGE_NAME | sed 's/\//\\\//g')
@@ -39,36 +34,6 @@ DEBOUNCER_IMAGE_NAME_ESCAPED=$(echo $DEBOUNCER_IMAGE_NAME | sed 's/\//\\\//g')
 FUSEKI_IMAGE_NAME_ESCAPED=$(echo $FUSEKI_IMAGE_NAME | sed 's/\//\\\//g')
 KAFKA_IMAGE_NAME_ESCAPED=$(echo $KAFKA_IMAGE_NAME | sed 's/\//\\\//g')
 ELASTIC_SEARCH_IMAGE_NAME_ESCAPED=$(echo $ELASTIC_SEARCH_IMAGE_NAME | sed 's/\//\\\//g')
-
-for file in k8s/*; do
-  cat $file | \
-  sed "s/{{URL}}/$URL/g" | \
-  sed "s/{{INTERNAL_IP}}/$INTERNAL_IP/g" | \
-  sed "s/{{CLUSTER_NAME}}/$GKE_CLUSTER_NAME/g" | \
-  sed "s/{{APP_VERSION}}/$APP_VERSION/g" | \
-  sed "s/{{VESSEL_TAG}}/$VESSEL_TAG/g" | \
-  sed "s/{{INDEXER_IMAGE_NAME}}/$INDEXER_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{API_IMAGE_NAME}}/$API_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{GATEWAY_IMAGE_NAME}}/$GATEWAY_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{AUTH_IMAGE_NAME}}/$AUTH_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{CLIENT_IMAGE_NAME}}/$CLIENT_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{DEBOUNCER_IMAGE_NAME}}/$DEBOUNCER_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{FUSEKI_TAG}}/$FUSEKI_TAG/g" | \
-  sed "s/{{FUSEKI_IMAGE_NAME}}/$FUSEKI_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{REDIS_TAG}}/$REDIS_TAG/g" | \
-  sed "s/{{REDIS_IMAGE_NAME}}/$REDIS_IMAGE_NAME/g" | \
-  sed "s/{{ZOOKEEPER_TAG}}/$ZOOKEEPER_TAG/g" | \
-  sed "s/{{ZOOKEEPER_IMAGE_NAME}}/$ZOOKEEPER_IMAGE_NAME/g" | \
-  sed "s/{{KAFKA_TAG}}/$KAFKA_TAG/g" | \
-  sed "s/{{KAFKA_IMAGE_NAME}}/$KAFKA_IMAGE_NAME_ESCAPED/g" | \
-  sed "s/{{ELASTIC_SEARCH_TAG}}/$ELASTIC_SEARCH_TAG/g" | \
-  sed "s/{{ELASTIC_SEARCH_IMAGE_NAME}}/$ELASTIC_SEARCH_IMAGE_NAME_ESCAPED/g" > \
-  "../$GKE_CONFIG_DIR/$(basename $file)"
-
-  if [ "${file##*.}" == 'sh' ]; then
-    chmod a+x "../$GKE_CONFIG_DIR/$(basename $file)"
-  fi
-done
 
 # generate local development dc file
 content=$(cat local-dev.yaml)
