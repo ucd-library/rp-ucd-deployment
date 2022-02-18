e82c00d77d3cc4a33a5b97c30f2ecbbe96baa4ac
Quinn Hart <qjhart@ucdavis.edu>
2022-02-17 16:33:53 -0800

    bump revision.

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ fi
 
 # Main version number we are tagging the app with. Always update
 # this when you cut a new version of the app!
-APP_VERSION=v1.4.0-alpha.${BUILD_NUM}
+APP_VERSION=v1.6.0-alpha.${BUILD_NUM}
 
 ##
 # TAGS
